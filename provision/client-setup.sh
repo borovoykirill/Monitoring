ceb7b503222b228534748b21ac0770f9508418a7
Borovoy Kirill <borovoy.kirill@yandex.by>
2020-02-23 22:41:05 +0300

    Minor fix

--- provision/client-setup.sh
+++ provision/client-setup.sh
@@ -4,7 +4,7 @@
 sudo yum install -y openldap-clients nss-pam-ldapd
 
 # Timeout until the server is installed
-sleep 120s
+sleep 99s
 
 # Add client's VM in LDAP
 sudo authconfig --enableldap --enableldapauth --ldapserver=10.10.1.11 --ldapbasedn="dc=epam,dc=devopslab,dc=com" --enablemkhomedir --update
